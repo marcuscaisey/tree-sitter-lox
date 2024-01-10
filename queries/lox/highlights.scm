dfa078c2b802255361d5baf9ade9487167dd94e4
Marcus Caisey <marcus@teckna.com>
2024-01-10 00:55:48 +0000

    feat: add highlights query

---
+++ queries/lox/highlights.scm
@@ -0,0 +1,40 @@
+"print" @keyword
+
+(comment) @comment
+
+(number) @number
+
+(string) @string
+
+(boolean) @boolean
+
+(nil) @constant.builtin
+
+[
+  "!"
+  "=="
+  "!="
+  "<"
+  "<="
+  ">"
+  ">="
+  "+"
+  "-"
+  "*"
+  "/"
+] @operator
+
+[
+  ","
+  ";"
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+] @punctuation.bracket
+
+[
+  "?"
+  ":"
+] @conditional.ternary
